f06e73aff529394cba4687dff63721e31e86c3aa
ChelesteWang <1581446178@qq.com>
2022-03-12 18:30:57 +0800

    chore: format code

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,11 +5,11 @@ import { Red, Title, TitleRN, Wrapper } from "./index.style";
 
 export default function Index() {
   return (
-    <Wrapper className="index">
+    <Wrapper className='index'>
       <Title>Hello World</Title>
       {process.env.TARO_ENV !== "rn" && <Red>红色测试</Red>}
       {process.env.TARO_ENV === "rn" && (
-        <TitleRN className="test">
+        <TitleRN className='test'>
           <Text style={{ color: "red" }}>Hello React Native</Text>
         </TitleRN>
       )}
