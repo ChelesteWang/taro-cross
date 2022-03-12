e5c48c55bf14e5d499ff48fb989f4707df5c2250
ChelesteWang <1581446178@qq.com>
2022-03-12 17:55:45 +0800

    feat: 添加 RN 指定平台渲染

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native";
 import { Red, Title, TitleRN, Wrapper } from "./index.style";
+// import "./index.css";
 
 export default function Index() {
   return (
     <Wrapper className='index'>
       <Title>Hello World</Title>
       <Red>红色测试</Red>
-      {process.env.TARO_ENV === 'rn' && <TitleRN><Text>Hello React Native</Text></TitleRN>}
+      {process.env.TARO_ENV === "rn" && (
+        <TitleRN >
+          <Text>Hello React Native</Text>
+        </TitleRN>
+      )}
     </Wrapper>
   );
 }
